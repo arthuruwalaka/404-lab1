aceed46828d87fae8285bc330aa8ecef7aa8f12d
arthuruwalaka <55423381+arthuruwalaka@users.noreply.github.com>
2023-01-13 14:52:47 -0700

    lab 1 second

--- script.py
+++ script.py
@@ -2,3 +2,6 @@ import requests
 
 print(requests.__version__)
 print(requests.get("http://www.google.com/").status_code)
+
+out = requests.get("https://raw.githubusercontent.com/arthuruwalaka/404-lab1/main/script.py")
+print(out.text)
